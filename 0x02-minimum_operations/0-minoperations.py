0bf12c2a00b82354f91244fb2f1be10973904f72
Okungbowa Godspower <godspowerokungbowa0@gmail.com>
2024-03-21 23:00:54 +0100

    Minimum operations'

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Module for 0-minoperations"""
+
+'''
+In a text file, there is a single character H. Your text editor can execute only two operations in this file: Copy All and Paste. Given a number n, write a method that calculates the fewest number of operations needed to result in exactly n H characters in the file
+'''
+def minOperations(n):
